543cf39431ce2082b32c81bc2293e30b1d0f8bca
KateAlf <katrin.alf@gmail.com>
2019-06-20 13:50:00 +0400

    Линтит

--- js/main.js
+++ js/main.js
@@ -2,20 +2,20 @@
 var PHOTOS_NUMBER = 25;
 
 var userComments = ['Всё отлично!',
-                    'В целом всё неплохо. Но не всё.',
-                    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-                    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-                    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-                    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-                   ];
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+  ];
 
 var userNames = ['Семён',
-                'Степан',
-                'Ангелина',
-                'Мария',
-                'Афанасий',
-                'Тихон'
-                ];
+  'Степан',
+  'Ангелина',
+  'Мария',
+  'Афанасий',
+  'Тихон'
+  ];
 
 //Функция вычисления случайного числа
 
@@ -36,25 +36,25 @@ var createObjectComments = function () {
   var objectComments = [];
   for (var i = 0; i < userNames.length; i++) {
     objectComments.push({
-    avatar: 'img/avatar-'+ i + '.svg',
-    message: getComment(),
-    name: userNames[getRandom(i, userNames.length - 1)]
-   })
+      avatar: 'img/avatar-' + i + '.svg',
+      message: getComment(),
+      name: userNames[getRandom(i, userNames.length - 1)]
+    })
   }
   return objectComments;
 };
 
 // Функция создания объекта с фото
-var createUserPhotos = function (count) {
+var createUserPhotos = function(count) {
   var userPhotos = [];
   for (var i = 0; i <= PHOTOS_NUMBER; i++) {
     userPhotos.push({
-      url: 'photos/'+ i + '.jpg',
+      url: 'photos/' + i + '.jpg',
       likes: getRandom(15, 200),
       comments: getComment()
-    })
+    });
   }
- return userPhotos;
+  return userPhotos;
 };
 
 // Генерируем контент на странице
@@ -62,13 +62,13 @@ var createUserPhotos = function (count) {
 var photos = createUserPhotos(PHOTOS_NUMBER);
 
 var pictureTemplate = document.querySelector('#picture')
-    .content
-    .querySelector('.picture');
+  .content
+  .querySelector('.picture');
 
 var picturesTitleElement = document.querySelector('.pictures');
 var fragment = document.createDocumentFragment();
 
-var renderPhoto = function (photo) {
+var renderPhoto = function(photo) {
   var pictureElement = pictureTemplate.cloneNode(true);
 
   pictureElement.querySelector('.picture__img').src = photo.url;
